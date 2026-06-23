6d4153a4d329a6f2a311ad18d0232c5461e5792c
joindevopscloud <info@joindevops.com>
2026-06-23 20:36:08 +0530

    terraform

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  value       = aws_vpc.main.id
+}
+
+output "public_subnets" {
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnets" {
+  value       = aws_subnet.private[*].id
+}
+
+output "database_subnets" {
+  value       = aws_subnet.database[*].id
+}
+
+output "database_subnet_group_name" {
+    value = aws_db_subnet_group.roboshop.name
+}
