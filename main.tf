1cf317a42834588eeab90dd16a22a8bedca9cb86
joindevopscloud <info@joindevops.com>
2026-06-24 10:12:56 +0530

    terraform

--- main.tf
+++ main.tf
@@ -137,6 +137,12 @@ resource "aws_nat_gateway" "main" {
   depends_on = [aws_internet_gateway.main]
 }
 
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_internet_gateway.main.id
+}
+
 resource "aws_route" "private" {
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = "0.0.0.0/0"
